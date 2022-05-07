cc2d27cd0ab19d47c77b9b379d7b528f3f04fb96
João Victor de Souza <j.victor@sinova.com.br>
2022-05-07 20:35:18 -0300

    [Update]: finalizado a implementação do script.js

--- script.js
+++ script.js
@@ -2,50 +2,46 @@ let order = [];
 let clickedOrder = [];
 let score = 0;
 
-const green = 0;
-const red = 1;
-const yellow = 2;
-const blue = 3;
+//0 - green
+//1 - red
+//2 - yellow
+//3 - blue
 
-const blueZone = document.querySelector('.blue');
-const redZone = document.querySelector('.red');
-const greenZone = document.querySelector('.green');
-const yellowZone = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
 
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
-    
     order[order.length] = colorOrder;
     clickedOrder = [];
 
-    for (let i in order){
-        let elementoColor = createColorElement(order[i]);
-        lightColor(elementoColor, Number(i) + 1);
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
     }
 }
 
 let lightColor = (element, number) => {
-    time = time * 500;
-
+    number = number * 500;
     setTimeout(() => {
         element.classList.add('selected');
     }, number - 250);
-
     setTimeout(() => {
         element.classList.remove('selected');
-    });
+    }, number);
 }
 
 let checkOrder = () => {
-    for (let i in clickedOrder){
-        if (clickedOrder[i] != order[i]){
-            lose();
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
             break;
         }
     }
-
-    if (clickedOrder.length == order.length){
-        alert('Pontuação: ' + score + '\nVocê acertou! Iniciando próximo nível!');
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
         nextLevel();
     }
 }
@@ -56,7 +52,46 @@ let click = (color) => {
 
     setTimeout(() => {
         createColorElement(color).classList.remove('selected');
-    });
-
-    checkOrder();
+        checkOrder();
+    },250);
 }
+
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if (color == 2) {
+        return yellow;
+    } else if (color == 3) {
+        return blue;
+    }
+}
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () => {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+green.onclick = () => click(0);
+red.onclick = () => click(1);
+yellow.onclick = () => click(2);
+blue.onclick = () => click(3);
+
+
+playGame();
