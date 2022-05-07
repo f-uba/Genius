f9a2932fb357acd6e8e2d840a6595ae3793e9bae
João Victor de Souza <j.victor@sinova.com.br>
2022-05-07 17:57:12 -0300

    [Update]: inserido a função 'checkOrder' em script.js

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ let lightColor = (element, number) => {
         element.classList.remove('selected');
     })
 }
+
+let checkOrder = () => {
+    for (let i in clickedOrder){
+        if (clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+    }
+
+    if (clickedOrder.length == order.length){
+        alert('Pontuação: ' + score + '\nVocê acertou! Iniciando próximo nível!');
+        nextLevel();
+    }
+}
