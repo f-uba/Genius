c4895cb893609da99a119bb4f397ef3763ef6539
João Victor de Souza <j.victor@sinova.com.br>
2022-05-07 17:40:27 -0300

    [Add]: adicionado o arquivo script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+const green = 0;
+const red = 1;
+const yellow = 2;
+const blue = 3;
+
+const blueZone = document.querySelector('.blue');
+const redZone = document.querySelector('.red');
+const greenZone = document.querySelector('.green');
+const yellowZone = document.querySelector('.yellow');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for (let i in order){
+        let elementoColor = createColorElement(order[i]);
+        lightColor(elementoColor, Number(i) + 1);
+    }
+}
